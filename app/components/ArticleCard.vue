ccb89fdb30a5f1505417356f676862ec37dd4200
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-11-16 02:33:01 +0100

    refactor(script): standardize script tag syntax across components and layouts

--- app/components/ArticleCard.vue
+++ app/components/ArticleCard.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 	export type ArticleCardItem = {
 		title: string;
 		description: string;
--- app/components/CategorySelector.vue
+++ app/components/CategorySelector.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 	const props = defineProps<{
 		categoryLabel: string;
 		selected?: string;
