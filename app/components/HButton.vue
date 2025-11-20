8942b91f20235fe868cf88a0d1d39de0a8037ccb
banedPowell <68949513+banedPowell@users.noreply.github.com>
2025-11-20 19:56:40 +0100

    fix(HButton): add dark mode support to button borders

--- app/components/HButton.vue
+++ app/components/HButton.vue
@@ -10,7 +10,7 @@
 	<NuxtLink
 		v-if="to"
 		:to="to"
-		class="border-default bg-muted hover:bg-elevated hover:border-muted text-muted hover:text-default flex cursor-pointer items-center justify-center gap-1 border text-sm transition-colors select-none"
+		class="border-default bg-muted hover:bg-elevated hover:border-muted text-muted hover:text-default flex cursor-pointer items-center justify-center gap-1 border text-sm transition-colors select-none dark:border-[#303030]"
 		:class="!label && icon ? 'p-1' : 'px-3 py-0.5'"
 	>
 		<UIcon v-if="icon" :name="icon" class="mt-px" />
@@ -22,7 +22,7 @@
 
 	<button
 		v-else
-		class="border-default bg-muted hover:bg-elevated hover:border-muted text-muted hover:text-default flex items-center justify-center gap-1 border text-sm transition-colors select-none"
+		class="border-default bg-muted hover:bg-elevated hover:border-muted text-muted hover:text-default flex items-center justify-center gap-1 border text-sm transition-colors select-none dark:border-[#303030]"
 		:class="!label && icon ? 'p-1' : 'px-3 py-0.5'"
 	>
 		<UIcon v-if="icon" :name="icon" />
